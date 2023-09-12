9bcc835e16b8976bbbc619a42b79e82278a475fa
joaoxyz <jpreis808@gmail.com>
2023-09-11 22:23:13 -0300

    2/19 tests passed

--- dccthread.c
+++ dccthread.c
@@ -1,50 +1,45 @@
 #include <ucontext.h>
+#include <stdlib.h>
 #include "dccthread.h"
 #include "dlist.h"
-#include <stdlib.h>
 
-struct dccthread
-{
-  ucontext_t uc;
-  char *name;
-  void (*function)(int);
-  int param;
+struct dccthread {
+    ucontext_t *uc;
+    char *name;
 };
 
 struct dlist *threadList;
+dccthread_t *currentThread = NULL;
 
-void dccthread_init(void (*func)(int), int param)
-{
-  threadList = dlist_create();
-  gerente 
-  principal
-
+void dccthread_init(void (*func)(int), int param) {
+    threadList = dlist_create();
+    dccthread_create("gerente", NULL, 0);
+    setcontext(dccthread_create("principal", func, param)->uc);
 }
 
-dccthread_t *dccthread_create(const char *name, void (*func)(int), int param)
-{
-  dccthread_t *newThread;
-  newThread->name = name;
-  newThread->function = func;
-  newThread->param = param;
-  // newThread->uc = ??;
-  getcontext(newThread->uc);
-  // stack_t *stack_pointer = malloc(sizeof(SIGSTKSZ));
-  newThread->uc->uc_stack = malloc(sizeof(SIGSTKSZ));
-  dlist_push_right(threadList, newThread);
+dccthread_t *dccthread_create(const char *name, void (*func)(int), int param) {
+    dccthread_t *newThread = malloc(sizeof (dccthread_t));
+    newThread->uc = malloc(sizeof (ucontext_t));
+    newThread->name = name;
+    getcontext(newThread->uc);
 
-  return newThread;
+    newThread->uc->uc_link = NULL; // set to manager thread
+    newThread->uc->uc_stack.ss_sp = (char *) malloc(THREAD_STACK_SIZE);
+    newThread->uc->uc_stack.ss_size = THREAD_STACK_SIZE;
+    makecontext(newThread->uc, func, 1, param);
+    
+    dlist_push_right(threadList, newThread);
+
+    return newThread;
 }
 
-void dccthread_yield(void)
-{
+void dccthread_yield(void) {
 }
 
-dccthread_t *dccthread_self(void)
-{
+dccthread_t *dccthread_self(void) {
+    return currentThread;
 }
 
-const char *dccthread_name(dccthread_t *tid)
-{
-  return tid->name;
+const char *dccthread_name(dccthread_t *tid) {
+    return tid->name;
 }
